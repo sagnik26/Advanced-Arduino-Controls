5483b5166e654af3d05c93b2b3e93702929147db
Sagnik Ghosh <52010932+sagnik26@users.noreply.github.com>
2019-08-12 15:02:52 +0530

    Add files via upload

---
+++ External_Inturrupt_1.ino
@@ -0,0 +1,27 @@
+const uint8_t btn=2;
+const uint8_t led=5;
+uint8_t led_state=LOW;
+
+void setup() {
+  // put your setup code here, to run once:
+DDRD = B00100000;
+PORTD = B00000100;
+
+Serial.begin(9600);
+
+attachInterrupt(digitalPinToInterrupt(btn),toggle,FALLING);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+
+delay(500);
+
+}
+
+int toggle()
+{
+  PORTD ^=(1 << led);
+  
+  }
